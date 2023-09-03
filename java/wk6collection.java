40ba1958f0ae874d1fd1683f6d79d872c9de13c0
ShauryaYamdagni.com <shauryayamdagni0@gmail.com>
2023-09-03 22:54:13 +0530

    first

--- java/wk6collection.java
+++ java/wk6collection.java
@@ -1,20 +1,14 @@
-         import java.util.Map;
-import java.util.TreeMap;
-         class Test{
-                public static void main (String[] args){
-                           Map<Integer, String> map = new TreeMap();
-                           Integer i1= new Integer(1);
-                           Integer i2= new Integer(2);
-                           Integer i3= new Integer(1);
-                           if(i1.equals(i3)){
-                                   map.put(i2,"MNO");
-                           }
-                           map.put(i1,"UVW");
-                           if(i1.equals(i3)){
-                            System.out.println("yahan aaya hai ");
-                                  map.put(i1,"ABC");
-                                  map.put(i2,"XYZ");
-                           }
-                          System.out.println(map);
-                 }
-          }
+class Example{
+     public static void main(String args[]) {
+         try {
+              System.out.println("Quotient is:" + 0/0);
+         }
+         catch(ArithmeticException e) {
+              System.out.println("Denominator should not be zero");
+              //System.exit(1);
+         }
+         finally{
+              System.out.println("you are in finally..");
+         }
+     }
+}
